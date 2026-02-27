4f595277aca9f4b5b372ac5f13de2650a05fe796
Amir <amirhamzar39@gmail.com>
2026-02-27 13:34:55 +0530

    Updated login page

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './app/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './lib/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
